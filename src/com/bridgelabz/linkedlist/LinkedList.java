faf71f4889f319c37da343f941a7aa7838abf0ae
PREETI2096 <preetirg96@gmail.com>
2022-11-04 13:48:47 +0530

    [preeti]: added uc4 insert elements in linkedlist

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -35,6 +35,20 @@ public void append(T data) {
 				tail =  node;
 			}
 		}
+public void insert(T data) {
+	Node <T> node = new Node(data);
+	if(head == null) {
+     	head = node;
+		//tail = node;
+	}else {
+		Node<T> temp = head;
+		while(temp.getNext() != null) {
+		   temp = temp.getNext();	         
+		}
+		temp.setNext(node);
+	}
+}
+	
 	
 public void show() {
 	if(head == null) {
@@ -49,37 +63,3 @@ public void show() {
 		}
 	}
 }
-//public void append(int new_data)
-//{
-//    /* 1. Allocate the Node &
-//       2. Put in the data
-//       3. Set next as null */
-//    Node new_node = new Node(new_data);
-//
-//    /* 4. If the Linked List is empty, then make the
-//          new node as head */
-//    if (head == null)
-//    {
-//        head = new Node(new_data);
-//        return;
-//    }
-//
-///* 4. This new node is going to be the last node, so
-//  make next of it as null */
-//new_node.next = null;
-//
-///* 5. Else traverse till the last node */
-//Node last = head; 
-//while (last.next != null)
-//last = last.next;
-//
-///* 6. Change the next of last node */
-//last.next = new_node;
-//return;
-//}
-//
-///* This function prints contents of linked list starting from
-//the given node */
-//
-//
-//
